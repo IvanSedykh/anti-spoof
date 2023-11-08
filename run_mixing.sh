7824c1dd34006a29c656974737c5e8114b9bd112
Ivan S <sed.ivan.dm@gmail.com>
2023-11-08 22:50:18 +0300

    full mixing

--- run_mixing.sh
+++ run_mixing.sh
@@ -1,10 +1,22 @@
+# train dataset
+echo "Building train"
 python mix.py \
-    --data_dir ../asr/data/datasets/librispeech/dev-clean/ \
-    --out_folder data/datasets/librispeech/dev-clean/ \
-    --n_files 20 \
+    --data_dir ../asr/data/datasets/librispeech/train-clean-100/ \
+    --out_folder data/datasets/librispeech/train-clean-100/ \
+    --n_files 25000 \
     --num_speakers 100 \
     --snr_levels [0] \
     --test False \
-    --num_workers 4
+    --num_workers 8
 
-# todo make full datasets
+
+# test dataset
+echo "Building test"
+python mix.py \
+    --data_dir ../asr/data/datasets/librispeech/dev-clean/ \
+    --out_folder data/datasets/librispeech/dev-clean/ \
+    --n_files 3000 \
+    --num_speakers 100 \
+    --snr_levels [0] \
+    --test False \
+    --num_workers 8
