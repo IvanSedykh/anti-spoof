907466cf03c3826654d4f5d92b3951c8e663875f
Ivan Sedykh <sed.ivan.dm@gmail.com>
2023-11-18 22:34:35 +0300

    dataset improve

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -3,6 +3,7 @@ from src.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from src.datasets.librispeech_dataset import LibrispeechDataset
 from src.datasets.common_voice import CommonVoiceDataset
 from src.datasets.librispeech_mix_dataset import LibrispeechMixDataset, CustomDirTestDataset
+from src.datasets.fastspeech_dataset import BufferDataset
 
 __all__ = [
     "LibrispeechDataset",
@@ -11,5 +12,6 @@ __all__ = [
     "LJspeechDataset",
     "CommonVoiceDataset",
     "LibrispeechMixDataset",
-    "CustomDirTestDataset"
+    "CustomDirTestDataset",
+    "BufferDataset",
 ]
